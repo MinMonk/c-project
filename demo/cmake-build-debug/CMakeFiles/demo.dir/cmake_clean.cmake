035f2141af80559d869f927d106e4665568e68d5
MinMonk <459506421@qq.com>
2021-06-29 10:42:44 +0800

    C语言string.h库练习demo提交

---
+++ demo/cmake-build-debug/CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/main.c.obj"
+  "demo.exe"
+  "demo.exe.manifest"
+  "demo.lib"
+  "demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
